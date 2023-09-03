f451b05ed94fa28d436860c884cb893f0bfff896
chenyz <ahchen-yz@outlook.com>
2023-09-03 16:39:31 +0800

    [MODIFY] 删除duplicated files，运行IR - highlevel IR

--- compiler/src/find_push_elements.cpp
+++
@@ -1,9 +0,0 @@
-#include "llvm-load.hpp"
-
-int main(int argc, char *argv[]) {
-    LLVMStore ir_store;
-
-    ir_store.load_directory(argv[1]);
-    ir_store.print_all_elements();
-    return 0;
-}
